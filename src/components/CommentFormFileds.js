50aa1ed06dcb6dca66fe71f8abd169cac47d9069
Indhumathi <indhumathi.nagulsamy@mallow-tech.com>
2020-06-25 20:12:14 +0530

    email validation added

--- src/components/CommentFormFileds.js
+++ src/components/CommentFormFileds.js
@@ -2,31 +2,17 @@ import React from "react";
 import { Form, Input } from "antd";
 
 function CommentFormFields() {
-  return (
-    <>
-      <Form.Item
-        label="Name"
-        name="name"
-        rules={[{ required: true, message: "Please input your name!" }]}
-      >
+  return <>
+      <Form.Item label="Name" name="name" rules={[{ required: true, message: "Please input your name!" }]}>
         <Input />
       </Form.Item>
 
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: "Please input your email!" }]}
-      >
+      <Form.Item label="Email" name="email" rules={[{ required: true, message: "Please input your email!" }, { type: 'email', message: "Please enter valid email!" }]}>
         <Input />
       </Form.Item>
-      <Form.Item
-        label="Body"
-        name="body"
-        rules={[{ required: true, message: "Please input your comment!" }]}
-      >
+      <Form.Item label="Body" name="body" rules={[{ required: true, message: "Please input your comment!" }]}>
         <Input.TextArea />
       </Form.Item>
-    </>
-  );
+    </>;
 }
 export default CommentFormFields;
